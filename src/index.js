55e7dadc8dc41f8b6d92e5b09a5a44801a809920
eelayoubi <>
2018-12-25 17:52:10 +0100

    feature: add appsynn config

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ import configuration from './aws-exports';
 
 const appSyncConfig = {
   aws_appsync_graphqlEndpoint:
-    'https://hudgahnbufbvvb7lano5lvg6wy.appsync-api.us-east-1.amazonaws.com/graphql',
-  aws_appsync_region: 'us-east-1',
-  aws_appsync_authenticationType: 'API_KEY',
-  aws_appsync_apiKey: 'da2-w2ipm2v44rbsba736c3oqse2nm',
+  "https://eh6fmhsacndxlgnydapsugsqga.appsync-api.us-east-1.amazonaws.com/graphql",
+  "aws_appsync_region": "us-east-1",
+  "aws_appsync_authenticationType": "API_KEY",
+  "aws_appsync_apiKey": "da2-cgdt5a7lz5d6nh6w4xbzeokggy"
 };
 
 Amplify.configure({ ...configuration, ...appSyncConfig });
